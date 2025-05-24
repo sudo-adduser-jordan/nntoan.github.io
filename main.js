4d87a8c1fea2426a8dad854c4f0bac26928b7354
jordan <sudo.sendmail.jordan@gmail.com>
2025-05-23 17:46:41 -0700

    Update CSS

--- main.js
+++ main.js
@@ -1,74 +1,74 @@
 var header = `
 
-[[g;#EEEEEE;]    |]                                               $$\\
-[[g;#EEEEEE;]    |]                                               $$ |
-[[g;#EEEEEE;]    |]                 $$\\  $$$$$$\\   $$$$$$\\   $$$$$$$ | $$$$$$\\  $$$$$$$\\   
-[[g;#EEEEEE;]    |]                 \\__|$$  __$$\\ $$  __$$\\ $$  __$$ | \\____$$\\ $$  __$$\\ 
-[[g;#EEEEEE;]    |]                 $$\\ $$ /  $$ |$$ |  \\__|$$ /  $$ | $$$$$$$ |$$ |  $$ |    
-[[g;#EEEEEE;]    |]                 $$ |$$ |  $$ |$$ |      $$ |  $$ |$$  __$$ |$$ |  $$ |      
-[[g;#EEEEEE;]    |]                 $$ |\\$$$$$$  |$$ |      \\$$$$$$$ |\\$$$$$$$ |$$ |  $$ |   
-[[g;#EEEEEE;]    |]                 $$ | \\______/ \\__|       \\_______| \\_______|\\__|  \\__|
-[[g;#EEEEEE;]    |]           $$\\   $$ |
-[[g;#EEEEEE;]    |]           \\$$$$$$  |
-[[g;#EEEEEE;]    |]            \\______/ 
-[[g;#EEEEEE;]    |]                      
-[[g;#EEEEEE;]    |]    [[g;#EEEEEE;]Software Developer]
-[[g;#EEEEEE;]    |]                                                                                                                    
+    |                                               $$\\
+    |                                               $$ |
+    |                 $$\\  $$$$$$\\   $$$$$$\\   $$$$$$$ | $$$$$$\\  $$$$$$$\\   
+    |                 \\__|$$  __$$\\ $$  __$$\\ $$  __$$ | \\____$$\\ $$  __$$\\ 
+    |                 $$\\ $$ /  $$ |$$ |  \\__|$$ /  $$ | $$$$$$$ |$$ |  $$ |    
+    |                 $$ |$$ |  $$ |$$ |      $$ |  $$ |$$  __$$ |$$ |  $$ |      
+    |                 $$ |\\$$$$$$  |$$ |      \\$$$$$$$ |\\$$$$$$$ |$$ |  $$ |   
+    |                 $$ | \\______/ \\__|       \\_______| \\_______|\\__|  \\__|
+    |           $$\\   $$ |
+    |           \\$$$$$$  |
+    |            \\______/ 
+    |                      
+    |    Software Developer
+    |                                                                                                                    
 `
 var menu_banner = `
-[[g;#EEEEEE;]    |]
-[[g;#EEEEEE;]    |]    Please type [[g;#EEEEEE;]menu] for a list of commands.
-[[g;#EEEEEE;]    |]
+    |
+    |    Please type menu for a list of commands.
+    |
 `
 
 var motd = `
-[[g;#EEEEEE;]    |]
-[[g;#EEEEEE;]    |]    "A byte of time is a byte of gold, but you can't buy that byte of time, with a byte of gold.”
-[[g;#EEEEEE;]    |]
+    |
+    |    "A byte of time is a byte of gold, but you can't buy that byte of time, with a byte of gold.”
+    |
 `
 
 var system_info = `
-[[g;#EEEEEE;]    |]           
-[[g;#EEEEEE;]    |]                -/oyddmdhs+:.                      user1@computer1
-[[g;#EEEEEE;]    |]            -odNMMMMMMMMNNmhy+-'                   ---------------
-[[g;#EEEEEE;]    |]          -yNMMMMMMMMMMMNNNmmdhy+-                 [[g;#EEEEEE;]OS:] Gentoo Linux x86_64
-[[g;#EEEEEE;]    |]        'omMMMMMMMMMMMMNmdmmmmddhhy/'              [[g;#EEEEEE;]Kernel:] Linux 6.12.25-gentoo-dist
-[[g;#EEEEEE;]    |]        omMMMMMMMMMMMNhhyyyohmdddhhhdo'            [[g;#EEEEEE;]Packages:] 950 (emerge), 7 (flatpak)
-[[g;#EEEEEE;]    |]       .ydMMMMMMMMMMdhs++so/smdddhhhhdm+'          [[g;#EEEEEE;]Shell:] zsh 5.9
-[[g;#EEEEEE;]    |]        oyhdmNMMMMMMMNdyooydmddddhhhhyhNd.         [[g;#EEEEEE;]DE:] Xfce4 4.20
-[[g;#EEEEEE;]    |]         :oyhhdNNMMMMMMMNNNmmdddhhhhhyymMh         [[g;#EEEEEE;]WM:] Xfwm4 (X11)
-[[g;#EEEEEE;]    |]           .:+sydNMMMMMNNNmmmdddhhhhhhmMmy         [[g;#EEEEEE;]Theme:] Adwaita [GTK3]
-[[g;#EEEEEE;]    |]              /mMMMMMMNNNmmmdddhhhhhmMNhs:         [[g;#EEEEEE;]Icons:] win10x
-[[g;#EEEEEE;]    |]           'oNMMMMMMMNNNmmmddddhhdmMNhs+'          [[g;#EEEEEE;]Cursor:] Adwaita
-[[g;#EEEEEE;]    |]         'sNMMMMMMMMNNNmmmdddddmNMmhs/.            [[g;#EEEEEE;]Terminal:] xfce4-terminal 1.1.5
-[[g;#EEEEEE;]    |]        /NMMMMMMMMNNNNmmmdddmNMNdso:'              [[g;#EEEEEE;]Terminal Font:] NerdFont NF 
-[[g;#EEEEEE;]    |]       +MMMMMMMNNNNNmmmmdmNMNdso/-            
-[[g;#EEEEEE;]    |]       yMMNNNNNNNmmmmmNNMmhs+/-'              
-[[g;#EEEEEE;]    |]       /hMMNNNNNNNNMNdhs++/-'                 
-[[g;#EEEEEE;]    |]       '/ohdmmddhys+++/:.'                    
-[[g;#EEEEEE;]    |]         '-//////:--.                         
-[[g;#EEEEEE;]    |]           
+    |           
+    |                -/oyddmdhs+:.                      user1@computer1
+    |            -odNMMMMMMMMNNmhy+-'                   ---------------
+    |          -yNMMMMMMMMMMMNNNmmdhy+-                 OS: Gentoo Linux x86_64
+    |        'omMMMMMMMMMMMMNmdmmmmddhhy/'              Kernel: Linux 6.12.25-gentoo-dist
+    |        omMMMMMMMMMMMNhhyyyohmdddhhhdo'            Packages: 950 (emerge), 7 (flatpak)
+    |       .ydMMMMMMMMMMdhs++so/smdddhhhhdm+'          Shell: zsh 5.9
+    |        oyhdmNMMMMMMMNdyooydmddddhhhhyhNd.         DE: Xfce4 4.20
+    |         :oyhhdNNMMMMMMMNNNmmdddhhhhhyymMh         WM: Xfwm4 (X11)
+    |           .:+sydNMMMMMNNNmmmdddhhhhhhmMmy         Theme: Adwaita [GTK3
+    |              /mMMMMMMNNNmmmdddhhhhhmMNhs:         Icons: win10x
+    |           'oNMMMMMMMNNNmmmddddhhdmMNhs+'          Cursor: Adwaita
+    |         'sNMMMMMMMMNNNmmmdddddmNMmhs/.            Terminal: xfce4-terminal 1.1.5
+    |        /NMMMMMMMMNNNNmmmdddmNMNdso:'              Terminal Font: NerdFont NF 
+    |       +MMMMMMMNNNNNmmmmdmNMNdso/-            
+    |       yMMNNNNNNNmmmmmNNMmhs+/-'              
+    |       /hMMNNNNNNNNMNdhs++/-'                 
+    |       '/ohdmmddhys+++/:.'                    
+    |         '-//////:--.                         
+    |           
 `
 
 var Commands = {
     print_banner: false,
     menu: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]motd]               - Display Message of the Days');
+        this.echo('\t|   motd               - Display Message of the Days');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]about]              - Summary of me');
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]fastfetch]          - Get my dev environment value');
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]projects]           - Recent projects of mine');
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]skills]             - What I can do');
+        this.echo('\t|   about              - Summary of me');
+        this.echo('\t|   fastfetch          - Get my dev environment value');
+        this.echo('\t|   projects           - Recent projects of mine');
+        this.echo('\t|   skills             - What I can do');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]github]             - Das github');
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]linkedin]           - How I connect for work things');
+        this.echo('\t|   github             - Das github');
+        this.echo('\t|   linkedin           - How I connect for work things');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]contact]            - Contact me');
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]credits]            - Credits for this website');
+        this.echo('\t|   contact            - Contact me');
+        this.echo('\t|   credits            - Credits for this website');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]clear]              - Clear screen');
-        this.echo('\t[[g;#EEEEEE;]| ]  [[g;#EEEEEE;]all]                - Run all commands');
+        this.echo('\t|   clear              - Clear screen');
+        this.echo('\t|   all                - Run all commands');
         this.echo();
     },
     motd: function () {
@@ -76,15 +76,15 @@ var Commands = {
     },
     about: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Name:]         [[g;#EEEEEE;]Jordan]');
+        this.echo('\t|   Name:         Jordan');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Location:]     [[g;#EEEEEE;]United States, Remote]');
+        this.echo('\t|   Location:     United States, Remote');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Vocation:]     [[g;#EEEEEE;]Software Developer]');
-        this.echo('\t[[g;#EEEEEE;]| ]                Experienced in multiple languages, tools, and environments');
+        this.echo('\t|   Vocation:     Software Developer');
+        this.echo('\t|                 Experienced in multiple languages, tools, and environments');
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Employment:]   I am currently seeking a [[g;#EEEEEE;]Remote] position.');
-        this.echo('\t[[g;#EEEEEE;]| ]                Get in touch if you\'d like more information \t sudo.sendmail.jordan@gmail.com');
+        this.echo('\t|   Employment:   I am currently seeking a Remote position.');
+        this.echo('\t|                 Get in touch if you\'d like more information \t sudo.sendmail.jordan@gmail.com');
         this.echo();
     },
     fastfetch: function () {
@@ -93,15 +93,15 @@ var Commands = {
     },
     projects: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Code Racer] \t\t\t\t\t\t\t https://github.com/cody/code-racer');
-        this.echo('\t[[g;#EEEEEE;]|   Linux Themes] \t\t\t\t\t\t   https://github.com/linuxthemes');
-        this.echo('\t[[g;#EEEEEE;]|   Runelite Plugin] \t\t\t\t\t    https://github.com/runelite');
-        this.echo('\t[[g;#EEEEEE;]|   Bottles: Eve Online Launcher] \t\t  https://github.com/ohmybash/oh-my-bash');
+        this.echo('\t|   Code Racer \t\t\t\t\t\t\t https://github.com/cody/code-racer');
+        this.echo('\t|   Linux Themes \t\t\t\t\t\t   https://github.com/linuxthemes');
+        this.echo('\t|   Runelite Plugin \t\t\t\t\t    https://github.com/runelite');
+        this.echo('\t|   Bottles: Eve Online Launcher \t\t  https://github.com/ohmybash/oh-my-bash');
         this.echo();
     },
     skills: function () {
         this.echo();
-        this.echo('\t>  [[g;#EEEEEE;]Languages]');
+        this.echo('\t>  Languages');
         this.echo();
         this.echo('\t\t\t  \ue61d \t\t\t\t Javascript');
         this.echo('\t\t\t  \ue738 \t\t\t\t Java');
@@ -111,20 +111,20 @@ var Commands = {
         this.echo('\t\t\t  \ue61e \t\t\t\t C');
         this.echo('\t\t\t  \ue760 \t\t\t\t Bash');
         this.echo();
-        this.echo('\t>   [[g;#EEEEEE;]Frameworks]');
+        this.echo('\t>   Frameworks');
         this.echo();
         this.echo('\t\t\t  \ue7ba \t\t\t\t React');
         this.echo('\t\t\t  \ue8ac \t\t\t\t Spring');
         this.echo('\t\t\t  \udb81\udebf \t\t\t\t Angular');
         this.echo();
-        this.echo('\t>   [[g;#EEEEEE;]Platforms]');
+        this.echo('\t>   Platforms');
         this.echo();
         this.echo('\t\t\t  \ue7e6 \t\t\t\t Linux');
         this.echo('\t\t\t  \ue70f \t\t\t\t Windows');
         this.echo('\t\t\t  \uebaa \t\t\t\t Cloud');
         this.echo('\t\t\t  \uf21f \t\t\t\t Docker');
         this.echo();
-        this.echo('\t>   [[g;#EEEEEE;]Tools]');
+        this.echo('\t>   Tools');
         this.echo();
         this.echo('\t\t\t  \ue8da \t\t\t\t VSCode');
         this.echo('\t\t\t  \ue745 \t\t\t\t Librewolf');
@@ -133,38 +133,38 @@ var Commands = {
     },
     certifications: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Java Developer] \t\t\t\t https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Java Backend Developer] \t\t https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Java Desktop Developer] \t\t https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Java Spring Security] \t\t  https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Python Developer] \t\t\t   https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Go Developer]  \t\t\t\t   https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Front End Developer] \t\t    https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Javascript Developer] \t\t   https://github.com/sudo-adduser-jordan');
-        this.echo('\t[[g;#EEEEEE;]|   Linux Certified]  \t\t\t    https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Java Developer \t\t\t\t https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Java Backend Developer \t\t https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Java Desktop Developer \t\t https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Java Spring Security \t\t  https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Python Developer \t\t\t   https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Go Developer  \t\t\t\t   https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Front End Developer \t\t    https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Javascript Developer \t\t   https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Linux Certified  \t\t\t    https://github.com/sudo-adduser-jordan');
         this.echo();
     },
     github: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   https://github.com/sudo-adduser-jordan');
         this.echo();
     },
     linkedin: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]| ]  https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   https://github.com/sudo-adduser-jordan');
         this.echo();
     },
     contact: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Phone:]         [[g;#EEEEEE;]420.420.6969]');
-        this.echo('\t[[g;#EEEEEE;]|   Email:]         sudo.sendmail.jordan@gmail.com');
-        this.echo('\t[[g;#EEEEEE;]|   LinkedIn:]      https://github.com/sudo-adduser-jordan');
+        this.echo('\t|   Phone:         420.420.6969');
+        this.echo('\t|   Email:         sudo.sendmail.jordan@gmail.com');
+        this.echo('\t|   LinkedIn:      https://github.com/sudo-adduser-jordan');
         this.echo();
     },
     credits: function () {
         this.echo();
-        this.echo('\t[[g;#EEEEEE;]|   Forked and refactored from: ] https://github.com/nntoan/nntoan.github.io');
-        this.echo('\t[[g;#EEEEEE;]|   Using Jquery Terminal Emulator by Jakub Jankiewicz:] http://terminal.jcubic.pl');
+        this.echo('\t|   Forked and refactored from:  https://github.com/nntoan/nntoan.github.io');
+        this.echo('\t|   Using Jquery Terminal Emulator by Jakub Jankiewicz: http://terminal.jcubic.pl');
         this.echo(menu_banner);
     },
     all: function () {
@@ -193,7 +193,7 @@ jQuery(function () {
     $('#terminal_').terminal(
         Commands,
         {
-            prompt: '[[g;#EEEEEE;]js]> ',
+            prompt: '[[g;#00FF00;]user1@computer1 [[g;#0000FF;]~ $] ',
             completion: true,
             checkArity: false,
             greetings: "",
